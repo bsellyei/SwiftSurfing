b822d7959b9c5d085b9823f2417570f11c89a340
bsellyei <sellyei.bence@gmail.com>
2021-03-17 23:27:04 +0100

    add data model sql

---
+++ Docs/data_model.sql
@@ -0,0 +1,76 @@
+CREATE TABLE `users` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `full_name` varchar(255),
+  `email` varchar(255)
+);
+
+CREATE TABLE `couches` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `owner_id` int,
+  `name` varchar(255),
+  `address` varchar(255),
+  `city` varchar(255),
+  `country` varchar(255),
+  `latitude` double,
+  `longitude` double,
+  `max_guests` int,
+  `description` varchar(255),
+  `image_url` varchar(255),
+  `tags` varchar(255),
+  `rating_id` int
+);
+
+CREATE TABLE `ratings` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `couch_id` int,
+  `value` int,
+  `comment` varchar(255),
+  `user_id` int
+);
+
+CREATE TABLE `reservations` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `couch_id` int,
+  `guest_id` int,
+  `owner_id` int,
+  `guests_number` int,
+  `fromDate` datetime,
+  `toDate` datetime
+);
+
+CREATE TABLE `conversations` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `last_message` int,
+  `from_id` int,
+  `to_id` int
+);
+
+CREATE TABLE `messages` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `conversations_id` int,
+  `sender` int,
+  `when` datetime,
+  `message` varchar(255)
+);
+
+ALTER TABLE `couches` ADD FOREIGN KEY (`owner_id`) REFERENCES `users` (`id`);
+
+ALTER TABLE `couches` ADD FOREIGN KEY (`rating_id`) REFERENCES `ratings` (`id`);
+
+ALTER TABLE `ratings` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+ALTER TABLE `reservations` ADD FOREIGN KEY (`couch_id`) REFERENCES `couches` (`id`);
+
+ALTER TABLE `reservations` ADD FOREIGN KEY (`guest_id`) REFERENCES `users` (`id`);
+
+ALTER TABLE `reservations` ADD FOREIGN KEY (`owner_id`) REFERENCES `users` (`id`);
+
+ALTER TABLE `conversations` ADD FOREIGN KEY (`last_message`) REFERENCES `messages` (`id`);
+
+ALTER TABLE `conversations` ADD FOREIGN KEY (`from_id`) REFERENCES `users` (`id`);
+
+ALTER TABLE `conversations` ADD FOREIGN KEY (`to_id`) REFERENCES `users` (`id`);
+
+ALTER TABLE `messages` ADD FOREIGN KEY (`conversations_id`) REFERENCES `conversations` (`id`);
+
+ALTER TABLE `messages` ADD FOREIGN KEY (`sender`) REFERENCES `users` (`id`);
